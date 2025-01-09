ef7b87f21b1b5f63ab0377465a20c854cd8ca5d5
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-09 12:47:51 +0600

    add assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'Res/routes/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,11 +12,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      getPages: AppRoutes.appRoutes(),
     );
   }
 }
