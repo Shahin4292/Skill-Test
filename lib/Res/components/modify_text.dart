83d69bc73ff22cb8943bb989536cd1c4f546157d
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-09 15:14:30 +0600

    add done

---
+++ lib/Res/components/modify_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+class ModifiedText extends StatelessWidget {
+  final String text;
+  final Color? color;
+  final double? size;
+  final FontWeight? fontWeight;
+
+  const ModifiedText(
+      {super.key,
+        required this.text,
+        this.color,
+        required this.size,
+        this.fontWeight});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(color: color, fontSize: size, fontWeight: fontWeight),
+    );
+  }
+}
---
+++ lib/ViewModel/home/home_controller.dart
@@ -0,0 +1,33 @@
+import 'package:get/get.dart';
+
+import '../../Model/property/property_model.dart';
+import '../../Repository/property_repository/property_repository.dart';
+
+class HomeController extends GetxController {
+  final PropertyRepository _repository = PropertyRepository();
+  var nearProperties = <Property>[].obs;
+  var bestForYouProperties = <Property>[].obs;
+  var selectedCategory = "House".obs;
+  final List<String> categories = [
+    "House",
+    "Apartment",
+    "Hotel",
+    "Villa",
+    "Cottage"
+  ];
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchProperties();
+  }
+
+  void selectCategory(String category) {
+    selectedCategory.value = category;
+  }
+
+  void fetchProperties() {
+    nearProperties.value = _repository.getNearProperties();
+    bestForYouProperties.value = _repository.getBestForYouProperties();
+  }
+}
