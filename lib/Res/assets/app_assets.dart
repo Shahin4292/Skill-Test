0fd5cc5ed1b1fd3668b0207af88b57771bb67c98
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-09 12:47:16 +0600

    add assets

---
+++ lib/Res/assets/app_assets.dart
@@ -0,0 +1,26 @@
+class AppAssets {
+  static const String notifications = "assets/icons/notifications.svg";
+  static const String downArrow = "assets/icons/down_arrow.svg";
+  static const String menu = "assets/icons/menu.svg";
+  static const String locationsIcon = "assets/icons/location.svg";
+  static const String dreamsvileHouse = "assets/png/dreamsvile_house.png";
+  static const String image3 = "assets/png/image3.png";
+  static const String bedrooms = "assets/png/bedroom.png";
+  static const String strill = "assets/png/strill.png";
+  static const String drawing = "assets/png/drawing.png";
+  static const String men = "assets/png/men.png";
+  static const String image2 = "assets/png/image2.png";
+  static const String image1 = "assets/png/image1.png";
+  static const String image4 = "assets/png/image4.png";
+  static const String image5 = "assets/png/image5.png";
+  static const String location = "assets/png/location.png";
+  static const String bathroom = "assets/icons/bathroom.svg";
+  static const String bedroom = "assets/icons/bedroom.svg";
+  static const String saveIcon = "assets/icons/saveIcon.svg";
+  static const String logOut = "assets/icons/logOut.svg";
+  static const String help = "assets/icons/help.svg";
+  static const String setting = "assets/icons/setting.svg";
+  static const String messages = "assets/icons/messages.svg";
+  static const String profile = "assets/icons/profile.svg";
+  static const String home = "assets/icons/home.svg";
+}
