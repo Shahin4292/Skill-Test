3fbb72a9ae6056a2d33b03702f9fe7b8f47c75aa
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-09 09:25:43 +0600

    search bar added

--- lib/View/home/home_view.dart
+++ lib/View/home/home_view.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:skill_test/View/home/widgets/apps_bar.dart';
+import '../../Res/colors/app_colors.dart';
 import '../../ViewModel/home/home_controller.dart';
+
 
 class HomeView extends StatelessWidget {
   HomeView({super.key});
@@ -18,6 +21,35 @@ class HomeView extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               AppsBar(),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                      child: SizedBox(
+                    height: 48,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        fillColor: AppColor.whiteSmoke,
+                        filled: true,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                        hintText: 'Search address, or near you',
+                        hintStyle: TextStyle(
+                            color: AppColor.darkGrey,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none),
+                        prefixIcon: Icon(
+                          CupertinoIcons.search,
+                          size: 16,
+                          color: AppColor.grey,
+                        ),
+                      ),
+                    ),
+                  )),
+                ],
+              ),
             ],
           ),
         ),
