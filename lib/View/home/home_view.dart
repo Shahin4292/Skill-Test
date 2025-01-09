faf0d6a4e49354a49d9f25b17577cc18c4b45b33
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-09 17:55:45 +0600

    home View all are done

--- lib/View/home/home_view.dart
+++ lib/View/home/home_view.dart
@@ -1,12 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:skill_test/View/home/widgets/apps_bar.dart';
 import 'package:skill_test/View/home/widgets/button_category.dart';
+import 'package:skill_test/View/home/widgets/property_Item.dart';
+import 'package:skill_test/View/home/widgets/property_list.dart';
 import 'package:skill_test/View/home/widgets/search_menu.dart';
-import '../../Res/assets/app_assets.dart';
-import '../../Res/colors/app_colors.dart';
+import 'package:skill_test/View/home/widgets/section_header.dart';
 import '../../ViewModel/home/home_controller.dart';
 
 class HomeView extends StatelessWidget {
@@ -17,9 +16,9 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.only(right: 20, left: 20),
-        child: SafeArea(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(right: 20, left: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -46,51 +45,50 @@ class HomeView extends StatelessWidget {
                   ),
                 );
               }),
-              // SingleChildScrollView(
-              //   scrollDirection: Axis.horizontal,
-              //   padding: EdgeInsets.symmetric(horizontal: 8),
-              //   child: Row(
-              //     children: ['House', 'Apartment', 'Hotel', 'Villa']
-              //         .map((category) => ElevatedButton(
-              //               onPressed: () {}, child: Text(category),
-              //               // title: category,
-              //               // isSelected: false,
-              //               // onTap: () {},
-              //             ))
-              //         .toList(),
-              //   ),
-              // ),
-              // SizedBox(height: 16),
-              // SectionHeader(
-              //   title: 'Near from you',
-              //   onSeeMore: () {},
-              // ),
-              // Obx(() => SingleChildScrollView(
-              //       scrollDirection: Axis.horizontal,
-              //       child: Row(
-              //         children: controller.nearProperties
-              //             .map((property) => SizedBox(
-              //                   width: 250,
-              //                   height: 400,
-              //                   child: PropertyCard(property: property),
-              //                 ))
-              //             .toList(),
-              //       ),
-              //     )),
-              // SizedBox(height: 16),
-              // SectionHeader(
-              //   title: 'Best for you',
-              //   onSeeMore: () {},
-              // ),
-              // Obx(() => ListView.builder(
-              //       shrinkWrap: true,
-              //       physics: NeverScrollableScrollPhysics(),
-              //       itemCount: controller.bestForYouProperties.length,
-              //       itemBuilder: (context, index) {
-              //         return PropertyCard(
-              //             property: controller.bestForYouProperties[index]);
-              //       },
-              //     )),
+              SizedBox(height: 27),
+              SectionHeader(
+                title: 'Near from you',
+                onSeeMore: () {},
+              ),
+              SizedBox(height: 24),
+              Obx(() => SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      spacing: 24,
+                      children: controller.nearProperties
+                          .map((property) => SizedBox(
+                                // width: 250,
+                                // height: 400,
+                                child: PropertyItem(property: property),
+                              ))
+                          .toList(),
+                    ),
+                  )),
+              SizedBox(
+                height: 30,
+              ),
+              SectionHeader(
+                title: 'Best for you',
+                onSeeMore: () {},
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Expanded(
+                child: Obx(() => SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: Column(
+                        spacing: 24,
+                        children: controller.bestForYouProperties
+                            .map((property) => SizedBox(
+                                  // width: 250,
+                                  // height: 400,
+                                  child: PropertyList(property: property),
+                                ))
+                            .toList(),
+                      ),
+                    )),
+              )
             ],
           ),
         ),
