d673abf4fbe7ce063149d0ee6f82a146341551d1
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-11 21:15:46 +0600

    updated

--- lib/res/components/modify_text.dart
+++ lib/res/components/modify_text.dart
@@ -8,16 +8,20 @@ class ModifiedText extends StatelessWidget {
 
   const ModifiedText(
       {super.key,
-        required this.text,
-        this.color,
-        required this.size,
-        this.fontWeight});
+      required this.text,
+      this.color,
+      required this.size,
+      this.fontWeight});
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: TextStyle(color: color, fontSize: size, fontWeight: fontWeight),
+      style: TextStyle(
+          color: color,
+          fontSize: size,
+          fontWeight: fontWeight,
+          fontFamily: 'RaleWayBold'),
     );
   }
 }
